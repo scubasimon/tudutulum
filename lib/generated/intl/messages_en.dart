10a7a90fbd944e73f971693a3a3d20771d5efe79
kienvk <kien.vukhac@ddsolution.vn>
2022-11-24 18:13:58 +0700

    update onboard view and login

---
+++ lib/generated/intl/messages_en.dart
@@ -0,0 +1,59 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a en locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'en';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "account_not_exist":
+            MessageLookupByLibrary.simpleMessage("Don\'t have an account?"),
+        "create_account":
+            MessageLookupByLibrary.simpleMessage("Create account"),
+        "email": MessageLookupByLibrary.simpleMessage("Email"),
+        "forgot_password":
+            MessageLookupByLibrary.simpleMessage("Forgot password?"),
+        "get_started": MessageLookupByLibrary.simpleMessage("Get Started"),
+        "login": MessageLookupByLibrary.simpleMessage("Login"),
+        "login_description": MessageLookupByLibrary.simpleMessage(
+            "Welcome back,\nSign in to view the deal"),
+        "maybe_later": MessageLookupByLibrary.simpleMessage("Maybe later"),
+        "next": MessageLookupByLibrary.simpleMessage("Next"),
+        "onboard_description_1":
+            MessageLookupByLibrary.simpleMessage("Your Tulum travel companion"),
+        "onboard_description_2": MessageLookupByLibrary.simpleMessage(
+            "Plan your trip\n- what tudu while you\'re in Tulum -"),
+        "onboard_description_3": MessageLookupByLibrary.simpleMessage(
+            "What\'s on in Tulum\n- where goto; events, big and small -"),
+        "onboard_description_4": MessageLookupByLibrary.simpleMessage(
+            "Sign-in to view local deals & discounts"),
+        "onboard_title_1":
+            MessageLookupByLibrary.simpleMessage("What are you  "),
+        "onboard_title_2":
+            MessageLookupByLibrary.simpleMessage("Your local directory"),
+        "onboard_title_3":
+            MessageLookupByLibrary.simpleMessage("Your local calendar"),
+        "onboard_title_4": MessageLookupByLibrary.simpleMessage(
+            "Your local exclusive savings"),
+        "password": MessageLookupByLibrary.simpleMessage("Password"),
+        "sign_in_other":
+            MessageLookupByLibrary.simpleMessage("Or sign up/in with"),
+        "skip": MessageLookupByLibrary.simpleMessage("Skip")
+      };
+}
